e1d42a22d0c073b9c6ad743b37573a6d60c5269c
Thilanka Rodrigo <65930513+Thilanka0824@users.noreply.github.com>
2022-11-29 08:30:25 -0800

    testing

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../Hooks/Auth";
-import { useNavigate } from "react-router-dom";
+import {IoCartOutline} from "react-icons/ai"
 
 const NavBar = ({}) => {
   const auth = useAuth();
